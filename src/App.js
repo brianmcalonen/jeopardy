4a5da0c8ed0f7aec1707c2d6c2e4c1483f17241e
Brian McAlonen <bmcalonen@indeed.com>
2023-05-12 19:17:06 -0500

    commenting out steps to take

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // write useEffect that hits the /api/random endpoint using axios 6 times
+  // put those category ids in an array, make sure theres no duplicates
+  // for each category, get the five clues
+  // render the gameboard, 6 columns, 5 clues each
+  // render the scoreboard, set to 0
+  // on click, open the modal
+  // display the question and input and button for submit
+  // on submit, show the correct answer
+  // toggle the colors based on correct answer
+  // update the score based on correct answer
+  // x to close modal, disable the clue square
+
+  return <div className="App">hi</div>;
 }
 
 export default App;
