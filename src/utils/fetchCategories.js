245df59a49b97d0d36178a488734e21bea134d13
Brian McAlonen <bmcalonen@indeed.com>
2023-05-14 15:04:35 -0500

    get clues from ids

--- src/utils/fetchCategories.js
+++ src/utils/fetchCategories.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 import { setCategories, setAllClues, setLoading } from "../redux/gameSlice";
-import { getClues } from "./getClues";
-import { unstable_renderSubtreeIntoContainer } from "react-dom";
 
 export const fetchCategories = () => async (dispatch) => {
   dispatch(setLoading(true));
@@ -10,17 +8,17 @@ export const fetchCategories = () => async (dispatch) => {
     const response = await axios.get(
       "https://jservice.io/api/random/?count=10"
     );
-    console.log("response", response);
+
     let idsArray = [];
 
     if (response.data.length === undefined) return;
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < response.data.length; i++) {
       const id = response.data[i].category_id;
       idsArray.push(id);
     }
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < response.data.length; i++) {
       if (!idsArray.includes(idsArray[i])) {
         idsArray.push(idsArray[i]);
       }
@@ -31,13 +29,58 @@ export const fetchCategories = () => async (dispatch) => {
     dispatch(setCategories(idsArray));
     dispatch(setLoading(false));
 
-    // Call getClues function with the category IDs obtained from the API
-    const result = await getClues(idsArray);
-
-    dispatch(setAllClues(result));
-    dispatch(setLoading(false));
+    getClues(dispatch, idsArray);
   } catch (error) {
     // console.log(error.message);
     dispatch(setLoading(false));
   }
+};
+
+const getClues = (dispatch, ids) => {
+  let allClues = [];
+
+  let index = 0;
+
+  const getNextClues = () => {
+    if (index >= ids.length) {
+      dispatch(setAllClues(allClues));
+      return;
+    }
+
+    axios
+      .get(`https://jservice.io/api/clues/?category=${ids[index]}`)
+      .then((response) => {
+        // sort based on clue value
+        let newArr = response.data.map((obj) => ({
+          ...obj,
+          value: obj.value !== null ? obj.value : 1000,
+        }));
+
+        newArr.sort((a, b) => a.value - b.value);
+
+        newArr = newArr.slice(0, 5);
+
+        console.log("\n");
+        console.log(newArr[0].category.title);
+
+        newArr.forEach((clue) => {
+          console.log(`$${clue.value}`);
+          console.log("Question:", clue.question);
+          console.log("Answer: ", clue.answer);
+        });
+
+        console.log("\n");
+
+        allClues.push(newArr);
+
+        index++;
+
+        getNextClues();
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  getNextClues();
 };
--- src/utils/getClues.js
+++ src/utils/getClues.js
@@ -1,48 +1,51 @@
 import axios from "axios";
 import { setCategories, setAllClues, setLoading } from "../redux/gameSlice";
 
-export const getClues = async (dispatch, ids) => {
-  const promises = [];
+export const getClues = (dispatch, ids) => {
+  let allClues = [];
 
-  for (let i = 0; i < ids.length; i++) {
-    const promise = axios.get(
-      `https://jservice.io/api/clues/?category=${ids[i]}`
-    );
-    promises.push(promise);
-  }
+  let index = 0;
 
-  try {
-    const responses = await Promise.all(promises);
-
-    const allClues = [];
-
-    responses.forEach((response) => {
-      // sort based on clue value
-      let newArr = response.data.map((obj) => ({
-        ...obj,
-        value: obj.value !== null ? obj.value : 1000,
-      }));
-
-      newArr.sort((a, b) => a.value - b.value);
-
-      newArr = newArr.slice(0, 5);
-
-      console.log("\n");
-      console.log(newArr[0].category.title);
-
-      newArr.forEach((clue) => {
-        console.log(`$${clue.value}`);
-        console.log("Question:", clue.question);
-        console.log("Answer: ", clue.answer);
-      });
-
-      console.log("\n");
-
-      allClues.push(newArr);
-
+  const getNextClues = () => {
+    if (index >= ids.length) {
       dispatch(setAllClues(allClues));
-    });
-  } catch (error) {
-    console.log(error.message);
-  }
+      return;
+    }
+
+    axios
+      .get(`https://jservice.io/api/clues/?category=${ids[index]}`)
+      .then((response) => {
+        // sort based on clue value
+        let newArr = response.data.map((obj) => ({
+          ...obj,
+          value: obj.value !== null ? obj.value : 1000,
+        }));
+
+        newArr.sort((a, b) => a.value - b.value);
+
+        newArr = newArr.slice(0, 5);
+
+        console.log("\n");
+        console.log(newArr[0].category.title);
+
+        newArr.forEach((clue) => {
+          console.log(`$${clue.value}`);
+          console.log("Question:", clue.question);
+          console.log("Answer: ", clue.answer);
+        });
+
+        console.log("\n");
+
+        allClues.push(newArr);
+
+        index++;
+
+        getNextClues();
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  getNextClues();
 };
